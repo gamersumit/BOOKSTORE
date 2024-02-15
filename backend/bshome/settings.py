16323476aa9f80fbb9f808ce9961740f1f3974dc
Sumit Aggarwal <saggarwal@innow8apps.com>
2024-02-15 16:29:14 +0530

    included restframework

--- backend/bshome/settings.py
+++ backend/bshome/settings.py
@@ -43,6 +43,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party apps
+    'rest_framework',
+    'rest_framework.authtoken',
+
+    # user defined apps
     'account',
 ]
 
